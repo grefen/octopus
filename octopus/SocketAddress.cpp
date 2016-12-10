3e545e3bda08fdbb0c9ed79149c240e9cb73e4cd
grefen <ccbp2003@163.com>
2016-12-10 22:47:17 +0800

    add string.h

--- octopus/SocketAddress.cpp
+++ octopus/SocketAddress.cpp
@@ -5,6 +5,13 @@ namespace Octopus {
 
 		SocketAddress::SocketAddress()
 		{
+			std::memset(&mAddress, 0, sizeof(mAddress));
+			mAddress.mAddr.sin_family = AF_INET;
+
+		}
+		SocketAddress::SocketAddress(const std::string& hostAddress, Octopus::UInt16 port)
+		{
+
 		}
 
 		SocketAddress::~SocketAddress()
--- octopus/SocketAddress.h
+++ octopus/SocketAddress.h
@@ -1,18 +1,58 @@
 #ifndef SOCKETADDRESS_H
 #define SOCKETADDRESS_H
 
+
+#include "Base.h"
+
+
 namespace Octopus {
 
 	namespace Core {
 
-		class SocketAddress
+		class SocketAddress : public Copyable
 		{
 		public:
 			//默认全为0的地址，IPV4地址
 			SocketAddress();
+			//IP 地址和端口号, IPV4地址或IPV6地址
+			SocketAddress(const std::string& hostAddress, Octopus::UInt16 port);
+			//IP 地址和端口号，IPV4地址或IPV6地址
+			//如：192.168.1.131:80， [::ffff:192.168.1.131]:6666， www.163.com:80
+			SocketAddress(const std::string& hostAddressAndPort);
+			//从通用地址构造
+			explicit SocketAddress(const struct sockaddr* addr, Octopus::UInt32 length);
 
+
+			//拷贝构造函数
+			SocketAddress(const SocketAddress& addr);
 
 			~SocketAddress();
+
+
+			SocketAddress& operator = (const SocketAddress& socketAddress);
+
+			std::string     host() const;
+			Octopus::UInt16 port() const;
+			const struct sockaddr* addr() const;
+			std::string     hostAndPort() const;
+
+			Octopus::UInt32 length() const;
+
+			int             family()const;
+
+			enum
+			{
+				MAX_ADDRESS_LENGTH = sizeof(struct sockaddr_in6)				
+			};
+
+		private:
+
+			union
+			{
+				struct sockaddr_in  mAddr;
+				struct sockaddr_in6 mAdr6;
+			}mAddress;
+
 		};
 
 
