4566c2439cb07303bd82123f901fbfc878ebd2da
grefen <ccbp2003@163.com>
2017-02-11 09:33:52 +0800

    eventhandler

---
+++ octopus/EventHandler.cpp
@@ -0,0 +1,169 @@
+#include "EventHandler.h"
+#include "Reactor.h"
+#include "Log.h"
+
+namespace Octopus {
+
+	namespace Core {
+
+		EventHandler::EventHandler(Reactor* rector, int fd)
+			: mRector(rector)
+			, mfd(fd)
+			, mevents(NONE_EVENT)
+			, mrevents(NONE_EVENT)
+			, mindex(-1)
+			, meventHandling(false)
+		{
+
+		}
+
+
+		EventHandler::~EventHandler()
+		{
+		}
+
+		void EventHandler::handleEvent(Timestamp receiveTime)
+		{
+			meventHandling = true;
+			LOG_TRACE << reventsToString();
+
+			if ((mrevents & POLLHUP) && !(mrevents & POLLIN))
+			{
+				if (mcloseCallback) mcloseCallback();
+			}
+
+			if (mrevents & POLLNVAL)
+			{
+				LOG_WARN << "fd = " << mfd << " handle_event() POLLNVAL";
+			}
+
+			if (mrevents & (POLLERR | POLLNVAL))
+			{
+				if (merrorCallback) merrorCallback();
+			}
+			if (mrevents & (POLLIN | POLLPRI | POLLRDHUP))
+			{
+				if (mreadCallback) mreadCallback(receiveTime);
+			}
+			if (mrevents & POLLOUT)
+			{
+				if (mwriteCallback) mwriteCallback();
+			}
+			meventHandling = false;
+		}
+
+		void EventHandler::setReadCallback(const ReadCallback& callback)
+		{
+			mreadCallback = callback;
+		}
+		void EventHandler::setWriteCallback(const CallBack& callback)
+		{
+			mwriteCallback = callback;
+		}
+		void EventHandler::setCloseCallback(const CallBack& callback)
+		{
+			mcloseCallback = callback;
+		}
+		void EventHandler::setErrorCallback(const CallBack& callback)
+		{
+			merrorCallback = callback;
+		}
+
+		int EventHandler::fd()
+		{
+			return mfd;
+		}
+		int EventHandler::getEvents()
+		{
+			return mevents;
+		}
+		void EventHandler::setReadEvents(int events)
+		{
+			mrevents = events;
+		}
+		void EventHandler::enableRead(bool enable)
+		{
+			if (enable)
+			{
+				mevents |= READ_EVENT;
+			}
+			else
+			{
+				mevents &= ~READ_EVENT;
+			}
+		}
+		void EventHandler::enableWrite(bool enable)
+		{
+			if (enable)
+			{
+				mevents |= WRITE_EVENT;
+			}
+			else
+			{
+				mevents &= ~WRITE_EVENT;
+			}
+		}
+
+		bool EventHandler::isEnableRead()
+		{
+			return mrevents & READ_EVENT;
+		}
+		bool EventHandler::isEnalbeWrite()
+		{
+			return mevents & WRITE_EVENT;
+		}
+		int  EventHandler::getIndex()
+		{
+			return mindex;
+		}
+		void EventHandler::setIndex(int idx)
+		{
+			mindex = idx;
+		}
+		Reactor* EventHandler::getReactor()
+		{
+			return mRector;
+		}
+		std::string EventHandler::reventsToString() const
+		{
+			return eventsToString(mfd, mrevents);
+		}
+
+		std::string EventHandler::eventsToString() const
+		{
+			return eventsToString(mfd, mevents);
+		}
+
+		std::string EventHandler::eventsToString(int fd, int ev)
+		{
+			std::ostringstream oss;
+			oss << fd << ": ";
+			if (ev & POLLIN)
+				oss << "IN ";
+			if (ev & POLLPRI)
+				oss << "PRI ";
+			if (ev & POLLOUT)
+				oss << "OUT ";
+			if (ev & POLLHUP)
+				oss << "HUP ";
+			if (ev & POLLRDHUP)
+				oss << "RDHUP ";
+			if (ev & POLLERR)
+				oss << "ERR ";
+			if (ev & POLLNVAL)
+				oss << "NVAL ";
+
+			return oss.str().c_str();
+		}
+
+		void EventHandler::remove()
+		{
+			mRector->removeHandler(this);
+		}
+		void EventHandler::update()
+		{
+			mRector->updateHandler(this);
+		}
+
+	}
+}
---
+++ octopus/EventHandler.h
@@ -0,0 +1,80 @@
+#ifndef EVENTHANDLER_H
+#define EVENTHANDLER_H
+
+#include "Base.h"
+#include "Timestamp.h"
+#include <poll.h>
+
+namespace Octopus {
+
+	namespace Core {
+
+		class Reactor;
+
+		class EventHandler : public Noncopyable
+		{
+		public:
+
+			typedef std::function<void()> CallBack;
+			typedef std::function<void(Timestamp)> ReadCallback;
+
+			const int NONE_EVENT = 0;
+			const int READ_EVENT = POLLIN | POLLPRI;
+			const int WRITE_EVENT = POLLOUT;
+
+			EventHandler(Reactor* rector, int fd);
+			~EventHandler();
+
+			void handleEvent(Timestamp receiveTime);
+
+			void setReadCallback(const ReadCallback&);
+			void setWriteCallback(const CallBack&);
+			void setCloseCallback(const CallBack&);
+			void setErrorCallback(const CallBack&);
+
+			int fd();
+			int getEvents();
+			void setReadEvents(int events);
+
+			bool isNoneEvent() const { return mevents == NONE_EVENT; }
+
+			void enableRead(bool enable);
+			void enableWrite(bool enable);
+
+			bool isEnableRead();
+			bool isEnalbeWrite();
+
+			int  getIndex();
+			void setIndex(int idx);
+
+			Reactor* getReactor();
+
+			void remove();
+			void update();
+			
+			std::string reventsToString() const;
+			std::string eventsToString() const;
+
+		private:
+			static std::string eventsToString(int fd, int ev);
+		private:
+
+			Reactor* mRector;
+			const int mfd;
+
+			ReadCallback  mreadCallback;
+			CallBack      mwriteCallback;
+			CallBack      mcloseCallback;
+			CallBack      merrorCallback;
+
+			int        mevents;
+			int        mrevents; 
+			int        mindex; 
+
+			bool       meventHandling;
+		};
+	}
+}
+
+#endif
+
